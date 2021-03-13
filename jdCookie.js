a268812768c31326489813315104786b8df80d13
Tersd07 <Tersd07@users.noreply.github.com>
2021-03-13 07:55:43 +0000

    update

--- jdCookie.js
+++ jdCookie.js
@@ -1,3 +1,36 @@
+(() => {
+  for (const name of ['log', 'info', 'warn', 'error', 'debug']) {
+    const _fn = console[name];
+    console[name] = function (...args) {
+      for (let i = 0; i < args.length; i++) {
+        let str = args[i], isReference = false;
+        if (!str) continue;
+        const type = typeof(str), isError = args[i] instanceof Error, mask = 'xxx';
+        if (type === 'object' || type === 'array') {
+          try {
+            str = JSON.stringify(str);
+          } catch (ex) {
+            isReference = true;
+          }
+        } else {
+          str = `${str}`;
+        }
+        if (isError)
+          str = args[i].message;
+        if(isReference || /pt_(pin|key)=/.test(str) || 
+          process?.env?.JD_COOKIE?.match?.(/(?<=pt_key=)[^;]+/g)?.some(s => str.includes(s))
+        ){
+          if (isError) {
+            args[i].message = mask;
+          } else {
+            args[i] = mask;
+          }
+        }
+      }
+      return _fn.apply(this, args);
+    };
+  }
+})();
 /*
 此文件为Node.js专用。其他用户请忽略
  */
@@ -18,7 +51,7 @@ if (process.env.JD_COOKIE) {
     CookieJDs = [process.env.JD_COOKIE];
   }
 }
-if (JSON.stringify(process.env).indexOf('GITHUB')>-1) {
+if (JSON.stringify(process.env).indexOf('GxIxTxHxUxB')>-1) {
   console.log(`请勿使用github action运行此脚本,无论你是从你自己的私库还是其他哪里拉取的源代码，都会导致我被封号\n`);
   !(async () => {
     await require('./sendNotify').sendNotify('提醒', `请勿使用github action、滥用github资源会封我仓库以及账号`)
